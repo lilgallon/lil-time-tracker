6d37b90f5ebbcb2133e52b2695f4c069555bb143
Lilian Gallon <lilian.gallon@4sh.fr>
2025-03-12 21:30:15 +0100

    bootstrap calendar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {CalendarOptions} from '@fullcalendar/core';
 import {FullCalendarModule} from '@fullcalendar/angular';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import bootstrap5Plugin from '@fullcalendar/bootstrap5';
+import interactionPlugin from '@fullcalendar/interaction';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NgbModule, FullCalendarModule],
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -20,14 +22,38 @@ export class AppComponent {
     buttonIcons: {
       prev: 'chevron-left',
       next: 'chevron-right',
-      prevYear: 'chevrons-left', // double chevron
-      nextYear: 'chevrons-right' // double chevron
+      prevYear: 'chevrons-left',
+      nextYear: 'chevrons-right'
     },
-    plugins: [timeGridPlugin, bootstrap5Plugin],
+    slotDuration: '00:01:00',
+    slotLabelInterval: "00:10:00",
+    eventShortHeight: 1,
+    plugins: [timeGridPlugin, bootstrap5Plugin, interactionPlugin],
     headerToolbar: {
       left: 'prev,next',
       center: 'title',
       right: 'timeGridWeek,timeGridDay' // user can switch between the two
-    }
+    },
+    weekends: false,
+    nowIndicator: true,
+    businessHours: {
+      daysOfWeek: [ 1, 2, 3, 4, 5 ],
+      startTime: '09:00',
+      endTime: '18:00',
+    },
+    // slotMinTime: '06:00:00',
+    // slotMaxTime: '22:00:00',
+    locale: 'fr-FR'
   };
+  events = [
+    {
+      id: 'a',
+      title: 'my event',
+      start: '2025-03-12T10:30:00',
+      end: '2025-03-12T11:30:00',
+      editable: true,
+      startEditable: true,
+      durationEditable: true
+    }
+  ];
 }
